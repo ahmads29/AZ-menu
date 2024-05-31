add916be9e8e481730cae2a6438d64b77e20940a
Ahmad Sharkawi <ahmadsharkawi59@gmail.com>
2024-06-01 01:25:57 +0300

    landing page without footer

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById('nextBtn').addEventListener('click', function() {
+    document.querySelector('.shark_menu-items-container').scrollBy({
+        left: 200, // Adjust the scroll distance
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('prevBtn').addEventListener('click', function() {
+    document.querySelector('.shark_menu-items-container').scrollBy({
+        left: -200, // Adjust the scroll distance
+        behavior: 'smooth'
+    });
+});
